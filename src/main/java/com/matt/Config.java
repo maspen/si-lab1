66793840bec0c3a75624f5cd2fed98402f82384b
maspen <mattaspen@gmail.com>
2016-01-07 14:59:46 -0800

    1st commit

---
+++ src/main/java/com/matt/Config.java
@@ -0,0 +1,10 @@
+package com.matt;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:META-INF/spring/si-components.xml")
+public class Config {
+	//
+}
